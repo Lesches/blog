e71a8a61d854fae2216ddd53c3167f0bd694604f
sholom <sholomls@gmail.com>
2019-05-06 11:26:06 -0400

    views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PostsComponent} from './view/main/posts/posts.component';
+import {PostComponent} from './view/main/post/post.component';
+import {NotFoundComponent} from './view/main/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'posts'},
+  {path: 'posts', component: PostsComponent},
+  {path: 'post', component: PostComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { PostsComponent } from './view/main/posts/posts.component';
+import { PostComponent } from './view/main/post/post.component';
+import { NotFoundComponent } from './view/main/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
