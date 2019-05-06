07e647356815246854dc44a63034a55078ecc3b6
sholom <sholomls@gmail.com>
2019-05-06 13:13:07 -0400

    getAllPosts() method

--- src/app/model/dummy.service.ts
+++ src/app/model/dummy.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import {Post} from './post';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DummyService {
+  posts = [
+    new Post ({
+      id: 0,
+      title: 'Make America Great Again',
+      author: 'President Trump',
+      body: 'In America we worship G-d'
+    }),
 
-  constructor() { }
+    new Post ({
+      id: 1,
+      title: 'Make America Great Again',
+      author: 'Tucker Carlson',
+      body: 'Facts work, why don\'t you get some?'
+    }),
+
+    new Post ({
+      id: 2,
+      title: 'Make America Great Again',
+      author: 'President Trump',
+      body: 'In America we worship G-d'
+    }),
+
+    new Post ({
+      id: 3,
+      title: 'Make America Great Again',
+      author: 'President Trump',
+      body: 'In America we worship G-d'
+    }),
+  ]
+  constructor() {
+
+  }
+
+  getAllPosts(): Observable<Post[]>{
+    return of(this.posts);
+  }
 }
--- src/app/model/post.ts
+++ src/app/model/post.ts
@@ -1,11 +1,13 @@
-export class Post{
+export class Post {
+  id: number;
   title: string;
   body: string;
   author: string;
   created: Date;
 
-  constructor(args?){
-    if(args){
+  constructor(args?) {
+    if (args) {
+      this.id = args.id;
       this.author = args.author;
       this.body = args.body;
       this.title = args.title;
